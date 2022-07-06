1d5ad4951542d48184daadfcf91b5f87d5bf3ddd
MatiasG11 <matiasguarnera@gmail.com>
2022-07-06 19:32:35 -0300

    First commit for my API

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')  //I'm using express, so I require expresss
+const app = express()  //Instead of typing express, I'm typing app
+const PORT = 8000
+
+const countries ={
+    "argentina":{
+        "name":"argentina",
+        "continent":"South America",
+        "capitalCity":"Buenos Aires"
+    },
+
+     "brazil":{
+        "name":"brazil",
+        "continent":"South America",
+        "capitalCity":"Brasilia"
+    },
+
+     "uruguay":{
+        "name":"uruguay",
+        "continent":"South America",
+        "capitalCity":"Montevideo"
+    },
+
+    "unknown":{
+        "name":"unknown",
+        "continent":"South America",
+        "capitalCity":"Montevideo"
+    }
+}
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + "/index.html")
+})
+
+app.get('/api/:country', (request,response)=>{
+    const countryName = request.params.country.toLowerCase()
+    if (countries[countryName]){
+    response.json(countries[countryName])
+    }else{
+        response.json(countries["unknown"])
+    }
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is now running smoothly on port ${PORT}, like democracy.`)
+})
